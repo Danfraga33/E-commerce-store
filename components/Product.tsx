fb1e760470267bc0a9158a93d198407b9643376d
Daniel Fraga <dan.fraga33@gmail.com>
2023-12-01 16:22:25 +0000

    feat: products component

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { FC } from 'react';
+// To link to product details
+import Link from 'next/link';
+//To get image
 
-const Product = () => {
-	return <div>Product</div>;
+interface ProductType {
+	product: {
+		image: any; // Change
+		name: string;
+		slug: {
+			[key: string]: string;
+		};
+		price: number;
+	};
+}
+
+import { urlFor } from '@/lib/client';
+
+const Product: FC<ProductType> = ({
+	product: { image, name, slug, price },
+}) => {
+	return (
+		<div>
+			<Link href={`/product/${slug.current}`}>
+				<div className="product-card">
+					<img
+						src={urlFor(image && image[0]).url()}
+						width={250}
+						height={250}
+						className="product-image"
+					/>
+					<p className="product-name">{name}</p>
+					<p className="product-name">${price}</p>
+				</div>
+			</Link>
+		</div>
+	);
 };
 
 export default Product;
