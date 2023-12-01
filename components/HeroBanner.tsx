fd90c74eff6c27933ed549ebd10f4b57639d99d4
Daniel Fraga <dan.fraga33@gmail.com>
2023-12-01 14:01:24 +0000

    style: imgs

---
+++ components/HeroBanner.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from 'react';
+import Link from 'next/link';
+import { urlFor } from '../lib/client';
+
+interface HeroBannerProps {
+	HeroBanner: {
+		smallText?: string;
+		midText?: string;
+		largeText?: string;
+		image?: any; // Adjust the type based on your data structure
+		buttonText?: string;
+		desc?: string;
+		smallTe?: string;
+	};
+}
+const HeroBanner: FC<HeroBannerProps> = ({ HeroBanner }) => {
+	console.log(HeroBanner);
+	return (
+		<div className="hero-banner-container">
+			<div>
+				<p className="beats-solo">{HeroBanner?.smallText}</p>
+			</div>
+			<h3>{HeroBanner?.midText}</h3>
+			<h1>{HeroBanner?.largeText}</h1>
+			<img
+				src={urlFor(HeroBanner?.image).url()}
+				alt="headphones"
+				className="hero-banner-image"
+			/>
+
+			<div>
+				<Link href="/product/ID">
+					<button type="button"> {HeroBanner?.buttonText}</button>
+				</Link>
+				<div className="desc">
+					<h5>{HeroBanner?.desc}</h5>
+					<p>{HeroBanner?.smallTe}</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default HeroBanner;
