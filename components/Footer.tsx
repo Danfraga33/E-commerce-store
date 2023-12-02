c56020e92a20bddda011a4f2445b454a15d7ce8f
Daniel Fraga <dan.fraga33@gmail.com>
2023-12-02 15:35:19 +0000

    feat: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,58 +1,14 @@
-import React, { FC } from 'react';
-import Link from 'next/link';
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 
-import { urlFor } from '@/lib/client';
-
-interface FooterInterface {
-	footerBanner: {
-		discount: string;
-		largeText1: string;
-		largeText2: string;
-		saleTime: string;
-		midText: string;
-		smallText: string;
-		image: any;
-		button: string;
-		product: string;
-		desc: string;
-	};
-}
-
-const Footer: FC<FooterInterface> = ({
-	footerBanner: {
-		discount,
-		largeText1,
-		largeText2,
-		saleTime,
-		midText,
-		smallText,
-		image,
-		button,
-		product,
-		desc,
-	},
-}: any) => {
+const Footer = () => {
 	return (
-		<div className="footer-banner-container">
-			<div className="banner-desc">
-				<div className="left">
-					<p>{discount}</p>
-					<h3>{largeText1}</h3>
-					<h3>{largeText2}</h3>
-					<p>{saleTime}</p>
-				</div>
-				<div className="right">
-					<p>{smallText}</p>
-					<h3>{midText}</h3>
-
-					<p>{desc}</p>
-					<Link href={`/product/${product}`}>
-						<button type="button">{button}</button>
-					</Link>
-				</div>
-				<img src={urlFor(image).url()} className="footer-banner-image" alt="" />
-			</div>
-			Footer
+		<div className="footer-container">
+			<p>2023 JSM Headphones. All rights reserved.</p>
+			<p className="icons">
+				<AiFillInstagram />
+				<AiOutlineTwitter />
+			</p>
 		</div>
 	);
 };
