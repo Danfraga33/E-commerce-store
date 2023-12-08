fc7412106e7e30da19c68990ff72b7d2ffdd722b
Daniel Fraga <dan.fraga33@gmail.com>
2023-12-08 16:43:00 +0000

    fix: fixed layout issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,9 +15,5 @@ type AppPropsWithLayout = AppProps & {
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
 	const getLayout = Component.getLayout ?? ((page) => page);
-	return (
-		<StateContext>
-			<Component {...pageProps} />
-		</StateContext>
-	);
+	return <StateContext>{getLayout(<Component {...pageProps} />)}</StateContext>;
 }
