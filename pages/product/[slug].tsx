4e18774389c58cae809811ece394f8c4bba70ba4
Daniel Fraga <dan.fraga33@gmail.com>
2023-12-08 16:30:24 +0000

    fix: removing navbar from slug.

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -42,39 +42,38 @@ const ProductDetails: NextPageWithLayout<ProductDetails> = (
 	if (!products) {
 		return <div>Loading...</div>; // or handle the loading state in another way
 	}
-	const [index, setIndex] = useState(0);
 	const { image, name, details, price } = product;
+	const [index, setIndex] = useState(0);
 	const { decQty, incQty, qty, onAdd }: any = useStateContext(); // Change
 	return (
 		<div>
-			<Navbar />
 			<div className="product-detail-container">
-				<div className="image-container">
-					<img
-						src={urlFor(image && image[index]).url()}
-						className="product-detail-image"
-					/>
-					<div>
-						<div className="small-image-container">
-							{image?.map((item: HTMLImageElement, i: number) => (
-								<img
-									key={i}
-									src={urlFor(item).url()}
-									alt={`Thumbnail ${i + 1}`}
-									onMouseEnter={() => setIndex(i)}
-									className={
-										i === index ? 'small-image selected-image' : 'small-image'
-									}
-								/>
-							))}
-						</div>
+				<div>
+					<div className="image-container">
+						<img
+							src={urlFor(image && image[index]).url()}
+							className="product-detail-image"
+						/>
+					</div>
+					<div className="small-images-container">
+						{image?.map((item: HTMLImageElement, i: number) => (
+							<img
+								key={i}
+								src={urlFor(item).url()}
+								alt={`Thumbnail ${i + 1}`}
+								onMouseEnter={() => setIndex(i)}
+								className={
+									i === index ? 'small-image selected-image' : 'small-image'
+								}
+							/>
+						))}
 					</div>
 				</div>
 
 				<div className="product-detail-desc">
 					<h1>{name}</h1>
 					<div className="reviews">
-						<div className="flex">
+						<div>
 							<AiFillStar />
 							<AiFillStar />
 							<AiFillStar />
@@ -86,16 +85,18 @@ const ProductDetails: NextPageWithLayout<ProductDetails> = (
 					<h4>Details: </h4>
 					<p>{details}</p>
 					<p className="price">${price}</p>
-					<h3>Quantity: </h3>
-					<p className="quantity-desc">
-						<span className="minus" onClick={decQty}>
-							<AiOutlineMinus />
-						</span>
-						<span className="num">{qty}</span>
-						<span className="plus" onClick={incQty}>
-							<AiOutlinePlus />
-						</span>
-					</p>
+					<div className="quantity">
+						<h3>Quantity:</h3>
+						<p className="quantity-desc">
+							<span className="minus" onClick={decQty}>
+								<AiOutlineMinus />
+							</span>
+							<span className="num">{qty}</span>
+							<span className="plus" onClick={incQty}>
+								<AiOutlinePlus />
+							</span>
+						</p>
+					</div>
 					<div className="buttons">
 						<button
 							type="button"
@@ -109,15 +110,15 @@ const ProductDetails: NextPageWithLayout<ProductDetails> = (
 						</button>
 					</div>
 				</div>
+			</div>
 
-				<div className="maylike-products-wrapper">
-					<h2>You may also like:</h2>
-					<div className="marquee">
-						<div className="maylike-products-container track">
-							{products.map((item: any) => (
-								<Product key={item.id} product={item} />
-							))}
-						</div>
+			<div className="maylike-products-wrapper">
+				<h2>You may also like:</h2>
+				<div className="marquee">
+					<div className="maylike-products-container track">
+						{products.map((item: any) => (
+							<Product key={item.id} product={item} />
+						))}
 					</div>
 				</div>
 			</div>
