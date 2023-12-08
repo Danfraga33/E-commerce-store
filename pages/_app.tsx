baf2cb4601324f773e26a6c25e2cc2c957f7dcc3
Daniel Fraga <dan.fraga33@gmail.com>
2023-12-08 16:28:51 +0000

    chore: removing toaster from _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import { StateContext } from '@/context/StateContext';
-import { Toaster } from 'react-hot-toast';
 import type { AppProps } from 'next/app';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
